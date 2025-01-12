f7d5d1ff72e48ff515478e2082d1e59f0049d262
skrevenge <57811892+skrevenge@users.noreply.github.com>
2025-01-12 18:19:53 -0300

    Create PlayerStats.js

---
+++ PlayerStats.js
@@ -0,0 +1,408 @@
+const PlayerStatsData = {
+    'Endou Mamoru': {
+        name: 'Mark Evans',
+        undubName: 'Endou Mamoru',
+        TP: 180,
+        FP: 150,
+        element: 'Earth',
+        shoot: 68,
+        strength: 71,
+        dribble: 54,
+        keeper: 89,
+        speed: 60,
+        portrait: '',
+        LvUpStatBoost: {
+            boost1: [2, 1, 1, 2, 0, 4, 0],
+            boost2: [1, 2, 0, 3, 1, 3, 1],
+            boost3: [2, 1, 1, 1, 0, 4, 1],
+            boost4: [1, 2, 0, 2, 1, 3, 0],
+            boost5: [2, 1, 1, 2, 0, 4, 1]
+        },
+        HissatsuRankN: ['God Hand'],
+        HissatsuRankR: ['God Hand', 'Nekketsu Punch'],
+        HissatsuRankSR: ['God Hand', 'Nekketsu Punch', 'Majin The Hand'],
+        HissatsuRankUR: ['God Hand', 'Nekketsu Punch', 'Majin The Hand', 'Fist of Justice', 'God Catch'],
+        HissatsuRankLeg: ['God Hand', 'Nekketsu Punch', 'Majin The Hand', 'Fist of Justice', 'God Catch', 'Omega The Hand'],
+        RankUpBoost: {
+            RankR: [12, 10, 7, 15, 5, 20, 8],
+            RankSR: [15, 12, 8, 18, 6, 20, 10],
+            RankUR: [18, 15, 10, 20, 8, 20, 12],
+            RankLeg: [20, 18, 12, 20, 10, 20, 15]
+        }
+    }
+        this.addPlayer(new Player({
+            name: 'Nathan Swift',
+            undubName: 'Kazemaru Ichirouta',
+            TP: 160,
+            FP: 130,
+            element: 'Wind',
+            shoot: 63,
+            strength: 54,
+            dribble: 79,
+            keeper: 45,
+            speed: 87,
+            portrait: '',
+            LvUpStatBoost: {
+                boost1: [2, 2, 1, 0, 3, 0, 4],
+                boost2: [1, 2, 0, 1, 4, 0, 3],
+                boost3: [2, 1, 1, 0, 3, 0, 4],
+                boost4: [1, 2, 1, 1, 3, 0, 3],
+                boost5: [2, 1, 0, 0, 4, 0, 4]
+            },
+            HissatsuRankN: ['Shippuu Dash'],
+            HissatsuRankR: ['Shippuu Dash', 'Bunshin Feint'],
+            HissatsuRankSR: ['Shippuu Dash', 'Bunshin Feint', 'Dance of The Wind God'],
+            HissatsuRankUR: ['Shippuu Dash', 'Bunshin Feint', 'Dance of The Wind God', 'Triple Dash', 'Fujin no Mai'],
+            HissatsuRankLeg: ['Shippuu Dash', 'Bunshin Feint', 'Dance of The Wind God', 'Triple Dash', 'Fujin no Mai', 'Dark Phoenix'],
+            RankUpBoost: {
+                RankR: [12, 10, 8, 5, 20, 5, 20],
+                RankSR: [15, 12, 10, 6, 20, 5, 20],
+                RankUR: [18, 15, 12, 8, 20, 5, 20],
+                RankLeg: [20, 18, 15, 10, 20, 5, 20]
+            }
+        }
+        this.addPlayer(new Player({
+            name: 'Jack Wallside',
+            undubName: 'Kabeyama Heigoro',
+            TP: 170,
+            FP: 160,
+            element: 'Earth',
+            shoot: 50,
+            strength: 89,
+            dribble: 51,
+            keeper: 55,
+            speed: 45,
+            portrait: '',
+            LvUpStatBoost: {
+                boost1: [2, 2, 0, 4, 0, 1, 0],
+                boost2: [1, 2, 1, 3, 1, 0, 1],
+                boost3: [2, 1, 0, 4, 0, 1, 0],
+                boost4: [1, 2, 0, 3, 1, 1, 0],
+                boost5: [2, 1, 1, 4, 0, 0, 1]
+            },
+            HissatsuRankN: ['The Wall'],
+            HissatsuRankR: ['The Wall', 'Mountain Crush'],
+            HissatsuRankSR: ['The Wall', 'Mountain Crush', 'Rock Wall Dam'],
+            HissatsuRankUR: ['The Wall', 'Mountain Crush', 'Rock Wall Dam', 'The Mountain', 'Mega Wall'],
+            HissatsuRankLeg: ['The Wall', 'Mountain Crush', 'Rock Wall Dam', 'The Mountain', 'Mega Wall', 'Perfect Tower'],
+            RankUpBoost: {
+                RankR: [12, 10, 5, 20, 5, 8, 5],
+                RankSR: [15, 12, 6, 20, 6, 10, 6],
+                RankUR: [18, 15, 8, 20, 8, 12, 8],
+                RankLeg: [20, 18, 10, 20, 10, 15, 10]
+            }
+        }
+        this.addPlayer(new Player({
+            name: 'Axel Blaze',
+            undubName: 'Gouenji Shuuya',
+            TP: 170,
+            FP: 140,
+            element: 'Fire',
+            shoot: 89,
+            strength: 72,
+            dribble: 76,
+            keeper: 45,
+            speed: 71,
+            portrait: '',
+            LvUpStatBoost: {
+                boost1: [2, 1, 4, 1, 2, 0, 1],
+                boost2: [1, 2, 3, 2, 1, 0, 2],
+                boost3: [2, 1, 4, 1, 2, 0, 1],
+                boost4: [1, 2, 3, 1, 2, 0, 2],
+                boost5: [2, 1, 4, 2, 1, 0, 1]
+            },
+            HissatsuRankN: ['Fire Tornado'],
+            HissatsuRankR: ['Fire Tornado', 'Dragon Crash'],
+            HissatsuRankSR: ['Fire Tornado', 'Dragon Crash', 'Bakunetsu Storm'],
+            HissatsuRankUR: ['Fire Tornado', 'Dragon Crash', 'Bakunetsu Storm', 'Maximum Fire', 'Bakunetsu Screw'],
+            HissatsuRankLeg: ['Fire Tornado', 'Dragon Crash', 'Bakunetsu Storm', 'Maximum Fire', 'Bakunetsu Screw', 'Fire Blizzard'],
+            RankUpBoost: {
+                RankR: [12, 10, 20, 8, 15, 5, 12],
+                RankSR: [15, 12, 20, 10, 18, 5, 15],
+                RankUR: [18, 15, 20, 12, 20, 5, 18],
+                RankLeg: [20, 18, 20, 15, 20, 5, 20]
+            }
+        }
+        this.addPlayer(new Player({
+            name: 'Tod Ironside',
+            undubName: 'Kurimatsu Teppei',
+            TP: 150,
+            FP: 120,
+            element: 'Wind',
+            shoot: 55,
+            strength: 51,
+            dribble: 72,
+            keeper: 45,
+            speed: 82,
+            portrait: '',
+            LvUpStatBoost: {
+                boost1: [1, 1, 0, 0, 3, 0, 4],
+                boost2: [2, 1, 1, 1, 2, 0, 3],
+                boost3: [1, 2, 0, 0, 3, 0, 4],
+                boost4: [1, 1, 1, 1, 3, 0, 3],
+                boost5: [2, 1, 0, 0, 3, 0, 4]
+            },
+            HissatsuRankN: ['Dash Storm'],
+            HissatsuRankR: ['Dash Storm', 'Quick Drive'],
+            HissatsuRankSR: ['Dash Storm', 'Quick Drive', 'Running Wind'],
+            HissatsuRankUR: ['Dash Storm', 'Quick Drive', 'Running Wind', 'Maboroshi Dribble', 'Tatsumaki Senpuu'],
+            HissatsuRankLeg: ['Dash Storm', 'Quick Drive', 'Running Wind', 'Maboroshi Dribble', 'Tatsumaki Senpuu', 'True Wind'],
+            RankUpBoost: {
+                RankR: [10, 8, 8, 5, 15, 5, 20],
+                RankSR: [12, 10, 10, 6, 18, 5, 20],
+                RankUR: [15, 12, 12, 8, 20, 5, 20],
+                RankLeg: [18, 15, 15, 10, 20, 5, 20]
+            }
+        }
+        this.addPlayer(new Player({
+            name: 'Kevin Dragonfly',
+            undubName: 'Someoka Ryuugo',
+            TP: 165,
+            FP: 145,
+            element: 'Fire',
+            shoot: 85,
+            strength: 82,
+            dribble: 68,
+            keeper: 45,
+            speed: 66,
+            portrait: '',
+            LvUpStatBoost: {
+                boost1: [2, 1, 4, 2, 1, 0, 1],
+                boost2: [1, 2, 3, 3, 1, 0, 1],
+                boost3: [2, 1, 4, 2, 1, 0, 1],
+                boost4: [1, 2, 3, 3, 1, 0, 1],
+                boost5: [2, 1, 4, 2, 1, 0, 1]
+            },
+            HissatsuRankN: ['Dragon Crash'],
+            HissatsuRankR: ['Dragon Crash', 'Wyvern Crash'],
+            HissatsuRankSR: ['Dragon Crash', 'Wyvern Crash', 'Dragon Slayer'],
+            HissatsuRankUR: ['Dragon Crash', 'Wyvern Crash', 'Dragon Slayer', 'Wyvern Blizzard', 'Dragon Storm'],
+            HissatsuRankLeg: ['Dragon Crash', 'Wyvern Crash', 'Dragon Slayer', 'Wyvern Blizzard', 'Dragon Storm', 'Dark Dragon'],
+            RankUpBoost: {
+                RankR: [12, 10, 20, 15, 10, 5, 10],
+                RankSR: [15, 12, 20, 18, 12, 5, 12],
+                RankUR: [18, 15, 20, 20, 15, 5, 15],
+                RankLeg: [20, 18, 20, 20, 18, 5, 18]
+            }
+        }
+        this.addPlayer(new Player({
+            name: 'Jude Sharp',
+            undubName: 'Kidou Yuuto',
+            TP: 175,
+            FP: 155,
+            element: 'Wind',
+            shoot: 78,
+            strength: 68,
+            dribble: 89,
+            keeper: 45,
+            speed: 76,
+            portrait: '',
+            LvUpStatBoost: {
+                boost1: [2, 2, 1, 0, 4, 0, 2],
+                boost2: [1, 2, 2, 1, 3, 0, 2],
+                boost3: [2, 1, 1, 0, 4, 0, 2],
+                boost4: [1, 2, 2, 1, 3, 0, 2],
+                boost5: [2, 1, 1, 0, 4, 0, 2]
+            },
+            HissatsuRankN: ['Illusion Ball'],
+            HissatsuRankR: ['Illusion Ball', 'Twin Boost'],
+            HissatsuRankSR: ['Illusion Ball', 'Twin Boost', 'Killer Fields'],
+            HissatsuRankUR: ['Illusion Ball', 'Twin Boost', 'Killer Fields', 'Emperor Penguin No. 2', 'Death Zone'],
+            HissatsuRankLeg: ['Illusion Ball', 'Twin Boost', 'Killer Fields', 'Emperor Penguin No. 2', 'Death Zone', 'Emperor Penguin X'],
+            RankUpBoost: {
+                RankR: [12, 10, 15, 8, 20, 5, 12],
+                RankSR: [15, 12, 18, 10, 20, 5, 15],
+                RankUR: [18, 15, 20, 12, 20, 5, 18],
+                RankLeg: [20, 18, 20, 15, 20, 5, 20]
+            }
+        }
+        this.addPlayer(new Player({
+            name: 'David Samford',
+            undubName: 'Sakuma Jirou',
+            TP: 165,
+            FP: 140,
+            element: 'Wind',
+            shoot: 82,
+            strength: 65,
+            dribble: 76,
+            keeper: 45,
+            speed: 78,
+            portrait: '',
+            LvUpStatBoost: {
+                boost1: [2, 1, 3, 0, 2, 0, 3],
+                boost2: [1, 2, 4, 1, 1, 0, 2],
+                boost3: [2, 1, 3, 0, 2, 0, 3],
+                boost4: [1, 2, 4, 1, 1, 0, 2],
+                boost5: [2, 1, 3, 0, 2, 0, 3]
+            },
+            HissatsuRankN: ['Koutei Penguin 1go'],
+            HissatsuRankR: ['Koutei Penguin 1go', 'Koutei Penguin 2go'],
+            HissatsuRankSR: ['Koutei Penguin 1go', 'Koutei Penguin 2go', 'Koutei Penguin 3go'],
+            HissatsuRankUR: ['Koutei Penguin 1go', 'Koutei Penguin 2go', 'Koutei Penguin 3go', 'Death Zone', 'Twin Boost'],
+            HissatsuRankLeg: ['Koutei Penguin 1go', 'Koutei Penguin 2go', 'Koutei Penguin 3go', 'Death Zone', 'Twin Boost', 'Emperor Penguin X'],
+            RankUpBoost: {
+                RankR: [12, 10, 18, 8, 12, 5, 15],
+                RankSR: [15, 12, 20, 10, 15, 5, 18],
+                RankUR: [18, 15, 20, 12, 18, 5, 20],
+                RankLeg: [20, 18, 20, 15, 20, 5, 20]
+            }
+        }
+        this.addPlayer(new Player({
+            name: 'Joe King',
+            undubName: 'Genda Koujirou',
+            TP: 175,
+            FP: 145,
+            element: 'Wind',
+            shoot: 55,
+            strength: 76,
+            dribble: 52,
+            keeper: 87,
+            speed: 65,
+            portrait: '',
+            LvUpStatBoost: {
+                boost1: [2, 1, 0, 2, 0, 4, 1],
+                boost2: [1, 2, 1, 2, 1, 3, 1],
+                boost3: [2, 1, 0, 2, 0, 4, 1],
+                boost4: [1, 2, 1, 2, 1, 3, 1],
+                boost5: [2, 1, 0, 2, 0, 4, 1]
+            },
+            HissatsuRankN: ['Beast Fang'],
+            HissatsuRankR: ['Beast Fang', 'Triple Defense'],
+            HissatsuRankSR: ['Beast Fang', 'Triple Defense', 'Power Shield'],
+            HissatsuRankUR: ['Beast Fang', 'Triple Defense', 'Power Shield', 'Full Power Shield', 'Drill Smasher'],
+            HissatsuRankLeg: ['Beast Fang', 'Triple Defense', 'Power Shield', 'Full Power Shield', 'Drill Smasher', 'King Fire'],
+            RankUpBoost: {
+                RankR: [12, 10, 5, 15, 5, 20, 10],
+                RankSR: [15, 12, 6, 18, 6, 20, 12],
+                RankUR: [18, 15, 8, 20, 8, 20, 15],
+                RankLeg: [20, 18, 10, 20, 10, 20, 18]
+            }
+        }
+        this.addPlayer(new Player({
+            name: 'Sam Kincaid',
+            undubName: 'Gojou Masaru',
+            TP: 160,
+            FP: 135,
+            element: 'Earth',
+            shoot: 51,
+            strength: 85,
+            dribble: 55,
+            keeper: 52,
+            speed: 48,
+            portrait: '',
+            LvUpStatBoost: {
+                boost1: [2, 1, 0, 4, 1, 0, 1],
+                boost2: [1, 2, 1, 3, 1, 1, 0],
+                boost3: [2, 1, 0, 4, 1, 0, 1],
+                boost4: [1, 2, 1, 3, 1, 1, 0],
+                boost5: [2, 1, 0, 4, 1, 0, 1]
+            },
+            HissatsuRankN: ['Rock Wall'],
+            HissatsuRankR: ['Rock Wall', 'Stone Prison'],
+            HissatsuRankSR: ['Rock Wall', 'Stone Prison', 'Iron Wall'],
+            HissatsuRankUR: ['Rock Wall', 'Stone Prison', 'Iron Wall', 'The Wall', 'Mountain'],
+            HissatsuRankLeg: ['Rock Wall', 'Stone Prison', 'Iron Wall', 'The Wall', 'Mountain', 'Perfect Tower'],
+            RankUpBoost: {
+                RankR: [10, 8, 5, 20, 8, 5, 5],
+                RankSR: [12, 10, 6, 20, 10, 6, 6],
+                RankUR: [15, 12, 8, 20, 12, 8, 8],
+                RankLeg: [18, 15, 10, 20, 15, 10, 10]
+            }
+        }
+        this.addPlayer(new Player({
+            name: 'Jimmy Sixes',
+            undubName: 'Jimon Daiki',
+            TP: 155,
+            FP: 130,
+            element: 'Earth',
+            shoot: 48,
+            strength: 82,
+            dribble: 52,
+            keeper: 50,
+            speed: 45,
+            portrait: '',
+            LvUpStatBoost: {
+                boost1: [1, 2, 0, 4, 1, 0, 1],
+                boost2: [2, 1, 1, 3, 1, 1, 0],
+                boost3: [1, 2, 0, 4, 1, 0, 1],
+                boost4: [2, 1, 1, 3, 1, 1, 0],
+                boost5: [1, 2, 0, 4, 1, 0, 1]
+            },
+            HissatsuRankN: ['Dense Forest'],
+            HissatsuRankR: ['Dense Forest', 'Rock Wall'],
+            HissatsuRankSR: ['Dense Forest', 'Rock Wall', 'The Wall'],
+            HissatsuRankUR: ['Dense Forest', 'Rock Wall', 'The Wall', 'Mega Wall', 'Mountain Crush'],
+            HissatsuRankLeg: ['Dense Forest', 'Rock Wall', 'The Wall', 'Mega Wall', 'Mountain Crush', 'Perfect Tower'],
+            RankUpBoost: {
+                RankR: [10, 8, 5, 20, 8, 5, 5],
+                RankSR: [12, 10, 6, 20, 10, 6, 6],
+                RankUR: [15, 12, 8, 20, 12, 8, 8],
+                RankLeg: [18, 15, 10, 20, 15, 10, 10]
+            }
+        }
+        this.addPlayer(new Player({
+            name: 'Bobby Shearer',
+            undubName: 'Doumen Suichirou',
+            TP: 165,
+            FP: 140,
+            element: 'Wind',
+            shoot: 76,
+            strength: 62,
+            dribble: 84,
+            keeper: 45,
+            speed: 72,
+            portrait: '',
+            LvUpStatBoost: {
+                boost1: [2, 1, 2, 0, 4, 0, 2],
+                boost2: [1, 2, 2, 1, 3, 0, 2],
+                boost3: [2, 1, 2, 0, 4, 0, 2],
+                boost4: [1, 2, 2, 1, 3, 0, 2],
+                boost5: [2, 1, 2, 0, 4, 0, 2]
+            },
+            HissatsuRankN: ['Spiral Draw'],
+            HissatsuRankR: ['Spiral Draw', 'Killer Slide'],
+            HissatsuRankSR: ['Spiral Draw', 'Killer Slide', 'Wind Spider'],
+            HissatsuRankUR: ['Spiral Draw', 'Killer Slide', 'Wind Spider', 'Clone Defense', 'Hurricane Arrow'],
+            HissatsuRankLeg: ['Spiral Draw', 'Killer Slide', 'Wind Spider', 'Clone Defense', 'Hurricane Arrow', 'Perfect Wind'],
+            RankUpBoost: {
+                RankR: [12, 10, 15, 8, 20, 5, 12],
+                RankSR: [15, 12, 18, 10, 20, 5, 15],
+                RankUR: [18, 15, 20, 12, 20, 5, 18],
+                RankLeg: [20, 18, 20, 15, 20, 5, 20]
+            }
+        }
+        this.addPlayer(new Player({
+            name: 'Player Name',
+            undubName: 'Japanese Name',
+            TP: 150,
+            FP: 120,
+            element: 'Element',
+            shoot: 50,
+            strength: 50,
+            dribble: 50,
+            keeper: 50,
+            speed: 50,
+            portrait: '',
+            LvUpStatBoost: {
+                boost1: [1, 1, 1, 1, 1, 1, 1],
+                boost2: [1, 1, 1, 1, 1, 1, 1],
+                boost3: [1, 1, 1, 1, 1, 1, 1],
+                boost4: [1, 1, 1, 1, 1, 1, 1],
+                boost5: [1, 1, 1, 1, 1, 1, 1]
+            },
+            HissatsuRankN: ['Technique 1'],
+            HissatsuRankR: ['Technique 1', 'Technique 2'],
+            HissatsuRankSR: ['Technique 1', 'Technique 2', 'Technique 3'],
+            HissatsuRankUR: ['Technique 1', 'Technique 2', 'Technique 3', 'Technique 4', 'Technique 5'],
+            HissatsuRankLeg: ['Technique 1', 'Technique 2', 'Technique 3', 'Technique 4', 'Technique 5', 'Technique 6'],
+            RankUpBoost: {
+                RankR: [10, 8, 8, 8, 8, 8, 8],
+                RankSR: [12, 10, 10, 10, 10, 10, 10],
+                RankUR: [15, 12, 12, 12, 12, 12, 12],
+                RankLeg: [18, 15, 15, 15, 15, 15, 15]
+            }
+        }        
+    // Add other players here...
+};
+
+export default PlayerStatsData;
